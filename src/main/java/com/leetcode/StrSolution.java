41a190e7b66d8bed9c9fb49b0b8ed4df74b8574d
RomanticWd <402468285@qq.com>
2021-03-09 22:00:21 +0800

    1047. 删除字符串中的所有相邻重复项

--- src/main/java/com/leetcode/StrSolution.java
+++ src/main/java/com/leetcode/StrSolution.java
@@ -41,4 +41,61 @@ public class StrSolution {
         }
         return true;
     }
+
+    /**
+     * 132. 分割回文串 II
+     */
+    public int minCut(String s) {
+        int length = s.length();
+        char[] chars = s.toCharArray();
+
+        // st[i][j]表示下标i-j是不是回文字符串
+        boolean[][] st = new boolean[length][length];
+
+        // 初始化数组st[][]
+        for (int j = 0; j < length; j++) {
+            for (int i = j; i >= 0; i--) {
+                // 只有一个字符肯定是回文串
+                if (i == j) {
+                    st[i][j] = true;
+                } else {
+                    // 2个字符时候，且相同字符是回文串
+                    if (j - i + 1 == 2) {
+                        st[i][j] = (chars[i] == chars[j]);
+                    } else {
+                        // 长度大于2时候，满足st[i+1][j-1]=true && char[i]==char[j]时候，st[i][j]=true
+                        st[i][j] = (chars[i] == chars[j]) && (st[i + 1][j - 1]);
+                    }
+                }
+            }
+        }
+
+        // dp[i]表示下标为i的时候的回文串最小分割次数
+        int[] dp = new int[length];
+        for (int j = 1; j < length; j++) {
+            // 如果0-i是回文串，则无需分割
+            if (st[0][j]) {
+                dp[j] = 0;
+            } else {
+                // 类似于这种情况 aac,dp[0]=0,dp[1]=0,dp[2]=1; 这时候的c是占用一次分割次数的
+                dp[j] = dp[j - 1] + 1;
+
+                // 第 j 个字符本身不独立使用分割次数
+                // 举例：abbc,
+                // dp[0]=0,
+                // dp[1]=dp[0]+1=1
+                // 循环前：dp[2]=dp[1]+1=2,  循环后：dp[2] = min(2,2) = 2
+                // 循环前：dp[3]=dp[2]+1=3,  循环后：dp[2] = min(3,2) = 2
+                for (int i = 1; i < j; i++) {
+                    if (st[i][j]) {
+                        // 因为abbc中，bb是回文串，所以abb的回文串长度应该是2,
+                        // 同理 abcbd ，bcb是回文串，所以abcb的回文串长度应该是2
+                        dp[j] = Math.min(dp[j], dp[i - 1] + 1);
+                    }
+                }
+            }
+        }
+        // 最后一个字符的最小分割次数
+        return dp[length - 1];
+    }
 }
--- src/main/java/com/leetcode/stack/StackSolution.java
+++ src/main/java/com/leetcode/stack/StackSolution.java
@@ -6,7 +6,7 @@ public class StackSolution {
 
     public static void main(String[] args) {
         StackSolution solution = new StackSolution();
-        System.out.println(solution.backspaceCompare("ab#c", "ad#c"));
+        System.out.println(solution.removeDuplicates("abbaca"));
     }
 
     /**
@@ -32,6 +32,36 @@ public class StackSolution {
             }
         }
         return String.valueOf(stack);
+    }
+
+    /**
+     * 1047. 删除字符串中的所有相邻重复项
+     *
+     * @param S
+     */
+    public String removeDuplicates(String S) {
+        int length = S.length();
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < length; i++) {
+            char c = S.charAt(i);
+            if (stack.empty()) {
+                stack.push(c);
+            } else {
+                Character top = stack.peek();
+                if (top == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        // 直接String.valueOf(stack)返回的是[c, a]，需要处理一下返回格式
+        StringBuilder sb = new StringBuilder();
+        for (Character character : stack) {
+            sb.append(character);
+        }
+        return sb.toString();
     }
 
 }
