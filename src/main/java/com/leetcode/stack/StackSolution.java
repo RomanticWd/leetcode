31e73712683529a9e428bfc91d5c968f1ddab789
yue.liu <402468285@qq.com>
2021-03-11 14:01:13 +0800

    227. 基本计算器 II

--- src/main/java/com/leetcode/stack/StackSolution.java
+++ src/main/java/com/leetcode/stack/StackSolution.java
@@ -6,7 +6,7 @@ public class StackSolution {
 
     public static void main(String[] args) {
         StackSolution solution = new StackSolution();
-        System.out.println(solution.removeDuplicates("abbaca"));
+        System.out.println(solution.calculateII("3+2*2"));
     }
 
     /**
@@ -62,6 +62,113 @@ public class StackSolution {
             sb.append(character);
         }
         return sb.toString();
+    }
+
+    /**
+     * 224. 基本计算器
+     *
+     * @param s
+     * @return
+     */
+    public int calculate(String s) {
+        int length = s.length();
+        Stack<Integer> stack = new Stack<>();
+        // sign表示正负，正即是加，负即为减
+        int sign = 1;
+        int res = 0;
+
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            // 如果是数字
+            if (Character.isDigit(c)) {
+                // 当前的数字值
+                int cur = c - '0';
+                // 当下一位还是数字的时候，进位
+                while (i + 1 < length && Character.isDigit(s.charAt(i + 1))) {
+                    // 同时i自加1
+                    cur = cur * 10 + s.charAt(++i) - '0';
+                }
+                res = res + sign * cur;
+            } else if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c == '(') {
+                // 每次遇到（ 时候，将括号前的和与加减符号推入栈中，开始计算括号中的值
+                stack.push(res);
+                res = 0;
+                // 注意是先推入和，再推入符号
+                stack.push(sign);
+                sign = 1;
+            } else if (c == ')') {
+                // 遇到 ） 将堆栈中的数和符号出栈，由于之前是先推入数字，后推入符号，所以符号在栈顶
+                int sign2 = stack.pop();
+                int res2 = stack.pop();
+                // 当前值应该为 括号内的和res * 左括号前的符号sign2 + 左括号前的数字和res2
+                res = res2 + res * sign2;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 227. 基本计算器 II
+     *
+     * @param s
+     * @return
+     */
+    public int calculateII(String s) {
+        int length = s.length();
+        Stack<Integer> stack = new Stack<>();
+        // 预设第一个数字前是‘+’
+        int num = 0;
+        char preSign = '+';
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+
+            // 如果是数字
+            if (Character.isDigit(c)) {
+                // 当前的数字值
+                int cur = c - '0';
+                // 当下一位还是数字的时候，进位
+                while (i + 1 < length && Character.isDigit(s.charAt(i + 1))) {
+                    // 同时i自加1
+                    cur = cur * 10 + s.charAt(++i) - '0';
+                }
+                num = cur;
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == length - 1) {
+                // 不是数字时候，c不是空格或者c是最后一位
+                if (c != ' ' || i == length - 1) {
+                    switch (preSign) {
+                        case '+':
+                            // 加的时候，不需要优先计算，直接放入stack即可
+                            stack.push(num);
+                            break;
+                        case '-':
+                            // 减的时候，不需要优先计算，直接将负值放入stack即可
+                            stack.push(-num);
+                            break;
+                        case '*':
+                            // 乘的时候，需要优先计算，将栈顶数字取出做运算后再放入栈中
+                            int top = stack.pop();
+                            stack.push(top * num);
+                            break;
+                        case '/':
+                            stack.push(stack.pop() / num);
+                            break;
+                    }
+                    // 不是数字的时候，即为符号，将符号位preSign置为当前符号
+                    preSign = c;
+                    num = 0;
+                }
+            }
+        }
+        int res = 0;
+        for (int value : stack) {
+            res += value;
+        }
+        return res;
     }
 
 }
