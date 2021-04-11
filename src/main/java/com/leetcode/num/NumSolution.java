2ecf4300c95ccba97dca7b67d5da260edb0bb402
RomanticWd <402468285@qq.com>
2021-04-11 22:25:41 +0800

    264. 丑数 II

--- src/main/java/com/leetcode/num/NumSolution.java
+++ src/main/java/com/leetcode/num/NumSolution.java
@@ -1,5 +1,10 @@
 package com.leetcode.num;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
 /**
  * 数字相关题目
  */
@@ -7,7 +12,7 @@ public class NumSolution {
 
     public static void main(String[] args) {
         NumSolution solution = new NumSolution();
-        System.out.println(solution.isUgly(6));
+        System.out.println(solution.nthUglyNumber(10));
     }
 
     /**
@@ -30,4 +35,38 @@ public class NumSolution {
         return n == 1;
     }
 
+    /**
+     * 264. 丑数 II
+     */
+    public int nthUglyNumber(int n) {
+
+        // 1
+        // 存2,3,5 队列中1，2，3，5 出2
+        // 存2*2，2*3，2*5 => 4，6，10 队列中1，2，3，4，5，6，10 出3
+        // 存3*2，3*3，3*5 => 6，9，15 队列中1，2，3，4，5，6，9，10，15 出4
+        // 存4*2，4*3，4*5 => 8，12，20 队列中1，2，3，4，5，6，8，9，10，12，15，20 出5
+
+        int[] nums = new int[]{2, 3, 5};
+
+        // 因为题目说明1 <= n <= 1690，用int类型长度不够
+        Queue<Long> queue = new PriorityQueue<>();
+        Set<Long> set = new HashSet<>();
+        queue.add(1l);
+        set.add(1l);
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            // 取出栈顶最小元素
+            long curr = queue.poll();
+            res = (int) curr;
+            for (int num : nums) {
+                // set中没有说明不是重复的
+                if (!set.contains(curr * num)) {
+                    queue.add(curr * num);
+                    set.add(curr * num);
+                }
+            }
+        }
+        return res;
+    }
 }
