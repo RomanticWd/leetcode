1ec4f33443496ce1e8f82834a035c308da3a6062
yue.liu <402468285@qq.com>
2021-07-02 12:28:05 +0800

    168. Excel表列名称

--- src/main/java/com/leetcode/other/OtherSolution.java
+++ src/main/java/com/leetcode/other/OtherSolution.java
@@ -10,7 +10,7 @@ public class OtherSolution {
 
     public static void main(String[] args) {
         OtherSolution solution = new OtherSolution();
-        System.out.println(solution.climbStairs(2));
+        System.out.println(solution.convertToTitle(800));
     }
 
     /**
@@ -35,4 +35,28 @@ public class OtherSolution {
         return dp[n];
     }
 
+    /**
+     * 168. Excel表列名称
+     *
+     * @return java.lang.String
+     * @author yue.liu
+     * @since 2021/7/2 10:21
+     **/
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+
+        while (columnNumber > 0) {
+            // 比如数字是1，转为字母应该是'A'，应该是'A'+(1-1)，所以在转换前需要自减1
+            columnNumber--;
+            // 取余
+            int i = columnNumber % 26;
+            // 不转一次char的话最终存的会是数字
+            sb.append((char) (i + 'A'));
+            // 取整 最终取整会为0，所以while不能是 >= 0
+            columnNumber = columnNumber / 26;
+        }
+        // 由于是取余，是先从低位开始计算的，append后是放在前面，需要反序输出
+        sb.reverse();
+        return sb.toString();
+    }
 }
